ae4dadc264b24e09a37720864d212c381094d6a3
Zaman Hüseynli <zamanhuseynli23@gmail.com>
2025-03-04 18:59:00 +0400

    Update main.py

--- kriptex/main.py
+++ kriptex/main.py
@@ -1,8 +1,7 @@
-# main.py
 import sys
 from PyQt6.QtWidgets import QApplication
-from encryption import encrypt_file, decrypt_file  # Import the encryption module
-from kriptex_app import Kriptex  # Make sure you import your Kriptex class correctly
+from kriptex.kriptex_app import Kriptex  # Make sure you import your Kriptex class correctly
+from kriptex.encryption import encrypt_file, decrypt_file
 
 def main():
     app = QApplication(sys.argv)
@@ -11,4 +10,7 @@ def main():
     sys.exit(app.exec())  # Run the application's event loop
 
 if __name__ == "__main__":
-    main()  # Make sure to call the main function
+    main()
+
+
+
